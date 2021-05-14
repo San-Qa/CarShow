a2e44c18d1ff86068ae84ec0e5fcd0a15ec1e6ee
San-Qa <SMathy@qa.com>
2021-05-14 13:32:23 +0100

    completed index and connected to h2

---
+++ src/main/resources/user-data.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user` (`name`, `model`) VALUES ('BMW', 'M2');
---
+++ src/test/java/com/example/demo/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserControllerIntegrationTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
